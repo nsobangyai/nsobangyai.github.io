122e4f8cf29ebd6c9ad3a70d8e99b685739390d7
nsobangyai <nsobangyai@gmail.com>
2026-02-27 18:53:08 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,27 @@
-
+ <?php>
+  $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => 'https://opendata.moph.go.th/api/report_data',
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'POST',
+      CURLOPT_POSTFIELDS =>'{
+      "tableName": "s_dm_screen_pop_age",
+      "year": "2569",
+      "province": "11",
+      "type": "json"
+    }',
+      CURLOPT_HTTPHEADER => array(
+        'Content-Type: application/json'
+      ),
+    ));
+    
+    $response = curl_exec($curl);
+    
+    curl_close($curl);
+    echo $response;
+  ?>
